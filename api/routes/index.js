b7e0845c2b472b79d86787b0db2a17aff5532712
BeneharFranco <befraalo@gmail.com>
2024-06-14 18:30:14 +0100

    company router

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router()
 
 const jobOpeningRouter = require("./job_opening.router")
 const applicationRouter = require("./application.router")
+const companyRouter = require("./company.router")
 
 router.use('/jobOpening', jobOpeningRouter)
 router.use('/application', applicationRouter)
+router.use('./company', companyRouter)
 
 module.exports = router
