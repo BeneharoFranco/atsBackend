0f64b49b24818dc7cb1104556603f698b5302966
yeraki <yeraki@hotmail.com>
2024-06-17 19:00:20 +0100

    Fix User Model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -6,31 +6,37 @@ const User = connection.define(
   {
     first_name: {
       type: DataTypes.STRING,
-    },
-    first_name: {
-      type: DataTypes.STRING,
+      allowNull: false,
     },
     last_name: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     role: {
       type: DataTypes.ENUM("user", "admin"),
       defaultValue: "user",
+      allowNull: false,
     },
     email: {
       type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
     },
     phone: {
       type: DataTypes.INTEGER,
+      allowNull: false,
     },
     password: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     photo: {
       type: DataTypes.BLOB,
     },
     valid: {
       type: DataTypes.BOOLEAN,
+      defaultValue: true,
+      allowNull: false,
     },
   },
   {
