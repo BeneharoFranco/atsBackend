c2cf0eb06b090a2ebdd6f5b714fa6b1ca37be2b8
AytamiEugRod <aytaeugerodri@hotmail.com>
2024-06-17 20:14:45 +0100

    id application

--- database/relations.js
+++ database/relations.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const Candidate = require("../api/models/candidate.model");
 const JobOpening = require("../api/models/job_opening.model");
 const Application = require("../api/models/application.model");
@@ -34,32 +33,3 @@ const defineRelations = () => {
 module.exports = defineRelations;
 
 
-=======
-const Candidate = require("../api/models/candidate.model");
-const JobOpening = require("../api/models/job_opening.model");
-const Application = require("../api/models/application.model");
-const Company = require("../api/models/company.model");
-
-const defineRelations = () => {
-  //ONE TO ONE
-  //   User.hasOne(Contact)
-  //   Contact.belongsTo(User) //Will have the foreign key 'userId'
-
-  //ONE TO MANY
-  Company.hasMany(JobOpening, {onDelete: 'cascade', onUpdate: 'cascade'})
-  JobOpening.belongsTo(Company, {onDelete: 'cascade', onUpdate: 'cascade'})
-
-  // MANY TO MANY
-  Candidate.belongsToMany(JobOpening, { through: Application, onDelete: 'cascade', onUpdate: 'cascade' });
-  JobOpening.belongsToMany(Candidate, { through: Application, onDelete: 'cascade', onUpdate: 'cascade' });
-  
-
-
-  
-  console.log("Relations defined");
-};
-
-module.exports = defineRelations;
-
-
->>>>>>> dev
