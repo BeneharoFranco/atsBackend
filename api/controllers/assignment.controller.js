ff1c0dfca0c32e49e04b5877c9a49872bdf14488
AytamiEugRod <aytaeugerodri@hotmail.com>
2024-06-18 22:30:48 +0100

    ajuste assigment

--- api/controllers/assignment.controller.js
+++ api/controllers/assignment.controller.js
@@ -2,8 +2,6 @@ const Assignment = require("../models/assignment.model");
 const JobOpening = require("../models/job_opening.model");
 const User = require("../models/user.model");
 
-
-
 const getAllAssignments = async (req, res) => {
   try {
     const assignment = await Assignment.findAll();
@@ -28,20 +26,55 @@ const getOneAssignment = async (req, res) => {
   }
 };
 
-const createAssignment = async (req, res) => {//   FALTA 
+const createAssignment = async (req, res) => {
   try {
-    
+    const user = await User.findByPk(req.body.userId);
+    if (user == null) {
+      return res.status(404).send("User not found");
+    }
+
+    const jobOpening = await JobOpening.findByPk(req.body.jobOpeningId);
+    if (jobOpening == null) {
+      return res.status(404).send("JobOpening not found");
+    }
+
+    const assignment = await Assignment.create(req.body);
+    res.json({
+      result: assignment,
+    });
   } catch (error) {
     return res.status(500).send(error.message);
   }
 };
 
-async function updateAssignment(req, res) {//   FALTA
- try {
-    
- } catch (error) {
+async function updateAssignment(req, res) {
+  try {
+    if(req.body.userId){
+      const user = await User.findByPk(req.body.userId);
+      if (user == null) return res.status(404).send("User not found");
+    }
+
+    if(req.body.jobOpeningId){
+      const jobOpening = await JobOpening.findByPk(req.body.jobOpeningId);
+      if (jobOpening == null) return res.status(404).send("JobOpening not found");
+    }
+
+    const [assignmentExist, assignment] = await Assignment.update(req.body, {
+      returning: true,
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (assignmentExist !== 0) {
+      return res
+        .status(200)
+        .json({ message: "Assignment updated", assignment: assignment });
+    } else {
+      return res.status(404).send("Assignment not found");
+    }
+  } catch (error) {
     return res.status(500).send(error.message);
- }
+  }
 }
 
 async function deleteAssignment(req, res) {
