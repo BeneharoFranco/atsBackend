eb09c4216c961c0b6f4ed8c368c10305e9c73cdb
yeraki <yeraki@hotmail.com>
2024-06-19 21:06:22 +0100

    added CORS

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 
-
+const cors = require("cors");
 const { checkDBConnection, syncModels } = require("./database");
 const defineRelations = require("./database/relations");
 const router = require("./api/routes");
@@ -17,6 +17,14 @@ const app = express();
 app.use(express.json());
 
 app.use("/api", router); // http://localhost:3000/api/
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // origin allowed
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.listen(process.env.PORT, () => {
   console.log(`Express started, listening on port ${process.env.PORT}`);
