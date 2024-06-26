7dcac61f12321565104036271ae3a14bf9929212
AytamiEugRod <aytaeugerodri@hotmail.com>
2024-06-26 22:16:18 +0100

    ajuste post create candidate

--- api/routes/candidate.router.js
+++ api/routes/candidate.router.js
@@ -15,7 +15,8 @@ const {
 
 router.get('/', checkAuth, checkUser, getAllCandidates);
 router.get('/:id', checkAuth, checkUser, getOneCandidate);
-router.post('/', checkAuth, checkUser, createCandidate);
+router.post('/', createCandidate);
+// router.post('/', checkAuth, checkUser, createCandidate);
 router.put('/:id', checkAuth, checkUser, updateCandidate);
 router.delete('/:id', checkAuth, checkUser, deleteCandidate);
 
