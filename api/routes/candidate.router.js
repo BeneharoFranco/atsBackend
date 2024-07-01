507c5e8b9ebed3a9b8fe3eb333831a86eb7ade66
yeraki <yeraki@hotmail.com>
2024-07-01 08:50:04 +0100

    cand

--- api/routes/candidate.router.js
+++ api/routes/candidate.router.js
@@ -15,7 +15,7 @@ const {
 
 router.get('/', checkAuth, checkUser, getAllCandidates);
 router.get('/:id', checkAuth, checkUser, getOneCandidate);
-router.post('/', checkAuth, checkUser, createCandidate);
+router.post('/', /* checkAuth, checkUser, */ createCandidate);
 router.put('/:id', checkAuth, checkUser, updateCandidate);
 router.delete('/:id', checkAuth, checkUser, deleteCandidate);
 
